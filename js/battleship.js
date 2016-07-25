29b21a25acb3a432a3a106126f82ff3081a8e872
Oliver Sewell <o.sewell@hotmail.com>
2016-07-25 19:35:00 +0100

    Added logic for battleship game

--- js/battleship.js
+++ js/battleship.js
@@ -5,3 +5,23 @@ var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
+
+while (isSunk == false) {
+  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid cell number");
+  } else {
+    guesses = guesses + 1;
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+        alert("HIT");
+        hits = hits + 1;
+        if (hits == 3) {
+          isSunk = true;
+          alert("You sank my battleship");
+        }
+    } else {
+      alert('Miss');
+    }
+  }
+}
