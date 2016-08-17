877227d1c5395f4e6961f84858222ad88d619a78
Oliver Sewell <o.sewell@hotmail.com>
2016-08-17 14:55:38 +0100

    Implemented the View

--- js/battleship.js
+++ js/battleship.js
@@ -1,28 +1,20 @@
-var randomLoc = Math.floor(Math.random() * 5);
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+//view
 
-while (isSunk == false) {
-  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-  if (guess < 0 || guess > 6) {
-    alert("Please enter a valid cell number");
-  } else {
-    guesses = guesses + 1;
-
-    if (guess == location1 || guess == location2 || guess == location3) {
-        alert("HIT");
-        hits = hits + 1;
-        if (hits == 3) {
-          isSunk = true;
-          alert("You sank my battleship");
-        }
-    } else {
-      alert('Miss');
-    }
+var view = {
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class","hit");
+  },
+  displayMiss: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class","miss");
   }
-}
+};
+
+view.displayMiss("00");
+view.displayHit("24");
+view.displayMessage("Tap Tap, is this thing on?");
