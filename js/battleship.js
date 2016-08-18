0ef43f740100e22be46e074c1cf3b01d65bd4ba5
Oliver Sewell <o.sewell@hotmail.com>
2016-08-18 15:25:54 +0100

    Implementing the controller

--- js/battleship.js
+++ js/battleship.js
@@ -80,3 +80,18 @@ function parseGuess(guess) {
   }
   return null;
 }
+
+var controller = {
+  guesses: 0,
+
+  processGuess: function(guess) {
+    var location = parseGuess(guess);
+    if (location) {
+      this.guesses++;
+      var hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleships, in" + this.guesses + " guesses");
+      }
+    }
+  }
+}
