737078699d1f3dc6dc3b40edd63328914e867dbe
Oliver Sewell <o.sewell@hotmail.com>
2016-08-19 20:13:09 +0100

    Generated random ships location

--- js/battleship.js
+++ js/battleship.js
@@ -23,9 +23,9 @@ var model = {
   numShips: 3,
   shipsSunk: 0,
   shipLength: 3,
-  ships: [ { locations: ["06", "16", "26"], hits: ["","",""] },
-           { locations: ["24", "34", "44"], hits: ["","",""] },
-           { locations: ["10", "11", "12"], hits: ["","",""] } ],
+  ships: [ { locations: ["0", "0", "0"], hits: ["","",""] },
+           { locations: ["0", "0", "0"], hits: ["","",""] },
+           { locations: ["0", "0", "0"], hits: ["","",""] } ],
   fire: function(guess) {
     for(var i = 0; i < this.numShips; i++) {
       var ship = this.ships[i];
@@ -53,7 +53,48 @@ var model = {
       }
     }
     return true;
-  }
+  },
+  generateShipLocations: function() {
+    var locations;
+    for(var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+  },
+  generateShip: function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+    if (direction === 1) { // horizontal
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+    } else { // vertical
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+
+    var newShipLocations = [];
+    for(var i = 0; i < this.shipLength; i++) {
+      if(direction === 1) {
+        newShipLocations.push(row + "" + (col + i));
+      } else {
+        newShipLocations.push((row + i) + "" + col);
+      }
+    }
+    return newShipLocations;
+  },
+  collision: function(locations) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			for (var j = 0; j < locations.length; j++) {
+				if (ship.locations.indexOf(locations[j]) >= 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 };
 
 
@@ -102,6 +143,19 @@ var controller = {
 function init() {
   var fireButton = document.getElementById("fireButton");
   fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById("guessInput");
+  guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
+}
+
+//key press handler
+function handleKeyPress(e) {
+  var fireButton = document.getElementById("fireButton");
+  if(e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
 }
 
 //function - getitng players guess from form
